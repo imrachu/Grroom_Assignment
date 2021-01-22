ead8ff7b38302b6b453cb2c09208db11f25198d9
imrachu <rachitkanaujiakvs@gmail.com>
2021-01-23 00:53:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CodeSearch from './components/CodeSearch';
+import NameSearch from './components/NameSearch.js';
+import CurrencySearch from './components/CurrencySearch.js';
+import LanguageSearch from './components/LanguageSearch.js';
+import CapitalSearch from './components/CapitalSearch.js';
+import CallingSearch from './components/CallingSearch.js';
+import RegionSearch from './components/RegionSearch.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Search(props) {
+  if (props.para === null) {
+    return <h1>Select Parameter</h1>
+  }
+  else if (props.para === "name") {
+    return (
+      <div>
+        <NameSearch />
+      </div>
+    );
+  }
+  else if (props.para === "code") {
+    return (
+      <div>
+        <CodeSearch />
+      </div>
+    );
+  }
+  else if (props.para === "currency") {
+    return (
+      <div>
+        <CurrencySearch />
+      </div>
+    );
+  }
+  else if (props.para === "language") {
+    return (
+      <div>
+        <LanguageSearch />
+      </div>
+    );
+  }
+  else if (props.para === "capital") {
+    return (
+      <div>
+        <CapitalSearch />
+      </div>
+    );
+  }
+  else if (props.para === "calling") {
+    return (
+      <div>
+        <CallingSearch />
+      </div>
+    );
+  }
+  else if (props.para === "region") {
+    return (
+      <div>
+        <RegionSearch />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { parameter: null };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick(event) {
+    this.setState(state => ({
+      parameter: event.target.value
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">  
+        <button value = "name" onClick={this.handleToggleClick}>
+          Name
+        </button>
+        <button value = "code" onClick={this.handleToggleClick}>
+          Code
+        </button>
+        <button value = "currency" onClick={this.handleToggleClick}>
+          Currency
+        </button>
+        <button value = "language" onClick={this.handleToggleClick}>
+          Language
+        </button>
+        <button value = "capital" onClick={this.handleToggleClick}>
+          Capital
+        </button>
+        <button value = "calling" onClick={this.handleToggleClick}>
+          Calling
+        </button>
+        <button value = "region" onClick={this.handleToggleClick}>
+          Region
+        </button>
+        <Search para={this.state.parameter} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CallingSearch.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import CountryList from './CountryList';
+
+const SearchPage = (props) => {
+  const [input, setInput] = useState('');
+  const [countryListDefault, setCountryListDefault] = useState();
+  const [countryList, setCountryList] = useState();
+
+  const fetchData = async () => {
+    return await fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data => {
+        setCountryList(data)
+        setCountryListDefault(data)
+      });
+  }
+
+  const updateInput = async (input) => {
+    const filtered = countryListDefault.filter(country => {
+      return country.callingCodes.toLowerCase().includes(input.toString().toLowerCase())
+    })
+    setInput(input);
+    setCountryList(filtered);
+  }  
+
+  useEffect(() => { fetchData() }, []);
+    return (
+      <div>
+        <div>
+          <input
+            key="random1"
+            value={input}
+            placeholder={"Enter Name"}
+            onChange={(e) => updateInput(e.target.value)}
+          />
+        </div>
+        <h1>Country List</h1>
+      <CountryList countryList={countryList} updateInput={updateInput} />
+      </div>
+    );  
+}
+
+export default SearchPage
+
